9bdf1180def63e5f8108a0e7d4ef9b749d7efaa6
esa <140386522+mahesaPinuntun@users.noreply.github.com>
2026-08-26 11:58:33 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -27,8 +27,8 @@ connectDB();
 
 // Apply Rate Limiter specifically to login & register endpoints
 
-app.get('/', (req, res)) => {
-  res.json({message:"auth-service is running" , port: process.env.PORT || 8001 });
+app.get('/', (req, res) => {
+  res.json({ message: "auth-service is running", port: process.env.PORT || 8001 });
 });
 app.post('/api/auth/register', authLimiter, authController.register);
 app.post('/api/auth/login', authLimiter, authController.login);
